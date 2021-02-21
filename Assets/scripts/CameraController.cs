b081aeacaa8ee5d56d9f792648b61f3e6d6020fa
TBailez <tomasbailez11@gmail.com>
2021-02-21 13:57:10 -0300

    Add o teste

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+//mira a camera no personagem
+    public Transform target;
+//quão longe do personagem a camera deve estar
+    public Vector3 offset;
+//desligar o offset
+    public bool useOffsetValues;
+//rodar a camera em volta do jogador
+    public float rotateSpeed;
+//pivot usado para não rotacionar o personagem ao mexer na camera
+    public Transform pivot;
+//Variaveis para alterar os angulos da camera direto no unity
+    public float maxViewAngle;
+    public float minViewAngle;
+//Variavel para inverter o eixo Y
+    public bool invertY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!useOffsetValues){
+        offset = target.position - transform.position;
+        }
+        //Isso faz com que o pivot passa a estar no target ao inves de a camera (para melhorar movimento)
+        pivot.transform.position = target.transform.position;
+        pivot.transform.parent = target.transform;
+        //Mantem o mouse travado no meio da dela, a seta desaparece da tela
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    // Alterei para Late Update para deixar mais liso o jogo
+    void LateUpdate()
+    {
+        //Pegar a posição x do mouse e rodar o target(personagem)
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.Rotate(0, horizontal, 0);
+        //Pegar a posição y do mouse e rodar o pivot(que agora está no personagem)
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+       // pivot.Rotate(-vertical, 0, 0);
+        //inverter o eixo y
+        if(invertY){
+            pivot.Rotate(vertical, 0, 0);
+        
+        }
+        else
+            {
+                pivot.Rotate(-vertical, 0, 0);
+            }
+        //Limitar a rotação da camera para cima e para baixo 
+        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f){
+            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+        }
+
+        //Limitar para a camera nao ir e volatar ao aproximar do target
+        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle){
+            pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+        }
+
+
+
+        //Mover a camera baseado na atual rotação do target e do offset original, relacionado ao horizontal
+        float desiredYAngle = target.eulerAngles.y;
+        //Mover a camera baseado na atual rotação do target(agora pivot) e do offset original, relacionado ao vertical
+        float desiredXAngle = pivot.eulerAngles.x;
+        //Utilizar a dimensão quaternaria W para a rotação do target(ver Debug)
+        Quaternion rotation  = Quaternion.Euler(desiredXAngle, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //Impede que a camera entre em baixo da terra
+        if(transform.position.y < target.position.y){
+            transform.position = new Vector3(transform.position.x, target.position.y - 0.5f, transform.position.z);
+        }
+
+        /*(offset automatico baseado na posição do terget e no offset dado)
+        transform.position = target.position - offset;*/
+        transform.LookAt(target);
+    }
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    //public Rigidbody theRB;
+    public float jumpForce;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  theRB = GetComponent<Rigidbody>();
+      controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Utiliza Rigid Body para dar caracteristicas fisicas ao target(mais interessante usar controller para personagens jogaveis)
+       /* theRB.velocity = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, theRB.velocity.y, Input.GetAxis("Vertical") * moveSpeed);
+        
+        if(Input.GetButtonDown("Jump")){
+            theRB.velocity = new Vector3(theRB.velocity.x, jumpForce, theRB.velocity.z);
+        }*/
+
+        //Mover o target(personagem), baseado em qual for o movimento horizontal ou vertical(seta pra frente sempre move para o norte)
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed);
+
+        //variavel auxiliar para lembrar o valor de y e não travar o pulo
+        float yStore = moveDirection.y;
+        //Mover o target com que a camera sempre seja a frente
+        moveDirection = (transform.forward * Input.GetAxis("Vertical")) + (transform.right * Input.GetAxis("Horizontal"));
+        //normaliza a velocidade na diagonal
+        moveDirection = moveDirection.normalized * moveSpeed;
+        //restora o valor original de y
+        moveDirection.y = yStore;
+
+        if (controller.isGrounded){
+            moveDirection.y = 0f;
+
+        if(Input.GetButtonDown("Jump")){
+            moveDirection.y = jumpForce;
+        }
+    }
+        moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale);
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
